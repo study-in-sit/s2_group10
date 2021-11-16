ffc36e7806c92b0cbc6f37c2ce22a7e6e566867a
pizzappch <68915844+pizzappch@users.noreply.github.com>
2021-11-16 14:20:49 +0700

    Update main.js

--- project/main.js
+++ project/main.js
@@ -218,6 +218,25 @@ search_btn.addEventListener('click', () => {
 
 remove_icon_ico.addEventListener('click', removeAllCartsList);
 
+//name repository
+let names = [];
+
+let name = {
+    name: document.getElementById('name').value
+}
+
+names.push(names);
+document.forms[0].reset();
+
+console.warn('added', {names});
+
+
+localStorage.setItem('addName', JSON.stringify(name));
+
+document.addEventListener('DOM', () => {
+    document.getElementById('btn').addEventListener('click', addName);
+});
+
 setCookie('name', 'Frong');
 showHideName();
 loadCartsHistory();
